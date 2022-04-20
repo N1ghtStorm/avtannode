2a899a9d5d0a577093efc86d99a65870324a1744
Dima Bushuev <diman1ghtly@gmail.com>
2022-04-20 21:12:58 +0200

    add first extrinsi

--- pallets/pallet-avtandb-kv/src/lib.rs
+++ pallets/pallet-avtandb-kv/src/lib.rs
@@ -58,10 +58,7 @@ pub mod pallet {
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		KeyAlreadyExists
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -69,41 +66,22 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-			// let who = ensure_signed(origin)?;
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1, 1))]
+		pub fn add_value(origin: OriginFor<T>, key: Vec<u8>, value: Vec<u8>) -> DispatchResult {
+			let caller = ensure_signed(origin)?;
+			KvStore::<T>::try_mutate(key, |val_opt| -> DispatchResult {
+				match val_opt {
+					Some(_) => {
+						return Err(Error::<T>::KeyAlreadyExists.into())
+					},
+					None => {
+						*val_opt = Some(value)
+					}
+				}
+				Ok(())
+			})?;
 
-			// // Update storage.
-			// <Something<T>>::put(something);
-
-			// // Emit an event.
-			// Self::deposit_event(Event::SomethingStored(something, who));
-			// // Return a successful DispatchResultWithPostInfo
 			Ok(())
-		}
-
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
-			Ok(())
-			// Read a value from storage.
-			// match <Something<T>>::get() {
-			// 	// Return an error if the value has not been set.
-			// 	None => Err(Error::<T>::NoneValue)?,
-			// 	Some(old) => {
-			// 		// Increment the value read from storage; will error in the event of overflow.
-			// 		// let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-			// 		// // Update the value in storage with the incremented result.
-			// 		// <Something<T>>::put(new);
-			// 		Ok(())
-			// 	},
-			// }
 		}
 	}
 }
