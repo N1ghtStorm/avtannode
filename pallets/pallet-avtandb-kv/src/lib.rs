c8de24a724bd29370fe40d69c91bbfd71e7ded4f
Dima Bushuev <diman1ghtly@gmail.com>
2022-05-14 18:59:15 +0200

    add offchain worker

--- pallets/pallet-avtandb-kv/src/lib.rs
+++ pallets/pallet-avtandb-kv/src/lib.rs
@@ -18,6 +18,17 @@ pub use pallet::*;
 pub mod pallet {
 	use frame_support::{dispatch::{DispatchResult, Vec}, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
+	use frame_support::sp_runtime::{
+		offchain as rt_offchain,
+		offchain::{
+			storage::StorageValueRef,
+			storage_lock::{BlockAndTime, StorageLock},
+		},
+		transaction_validity::{
+			InvalidTransaction, TransactionSource, TransactionValidity, ValidTransaction,
+		},
+		RuntimeDebug,
+	};
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -25,6 +36,25 @@ pub mod pallet {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 	}
+
+	#[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+        fn offchain_worker(_n: BlockNumberFor<T>) {
+            // frame_support::debug::print!("looool lLLLLLLLLLol lol lol {:?}", _n);
+			log::info!("================================================= {:?}", _n);
+			let a = match Self::fetch_from_remote(){
+				Err(e) => {
+					log::info!("=====ERROR:{:?}", e);
+					return;
+				},
+				Ok(v) => v
+			};
+
+			log::info!("ANSWER {:?}", a);
+            // debug::debug!("Entering off-chain workers {:?}", _n);
+            // todo!();
+        }
+    }
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -59,7 +89,8 @@ pub mod pallet {
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		KeyAlreadyExists
+		KeyAlreadyExists,
+		HttpFetchingError
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -83,6 +114,33 @@ pub mod pallet {
 			})?;
 			Self::deposit_event(Event::NewKeyAdded(key));
 			Ok(())
+		}
+	}
+
+	impl<T> Pallet<T> where T: Config {
+		fn fetch_from_remote() -> Result<Vec<u8>, Error<T>> {
+			let request = rt_offchain::http::Request::get("http://localhost:18085/avtan");
+
+			let timeout = sp_io::offchain::timestamp()
+				.add(rt_offchain::Duration::from_millis(3000));
+				let pending = request
+				// .add_header("User-Agent", HTTP_HEADER_USER_AGENT)
+				.deadline(timeout) // Setting the timeout time
+				.send() // Sending the request out by the host
+				.map_err(|_| <Error<T>>::HttpFetchingError)?;
+
+			let response = pending
+				.try_wait(timeout)
+				.map_err(|_| <Error<T>>::HttpFetchingError)?
+				.map_err(|_| <Error<T>>::HttpFetchingError)?;
+		  
+			if response.code != 200 {
+				// debug::error!("Unexpected http request status code: {}", response.code);
+				return Err(<Error<T>>::HttpFetchingError);
+			}
+		  
+			// Next we fully read the response body and collect it to a vector of bytes.
+			Ok(response.body().collect::<Vec<u8>>())
 		}
 	}
 }
